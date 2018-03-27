688a186c4518331f5005033aa9330c76820d6854
ypingcn <1344632698@qq.com>
2018-03-27 16:18:23 +0800

    refactor: rewrite utils using OOPs

--- TimeServerPlus/src/utility.cpp
+++ TimeServerPlus/src/utility.cpp
@@ -21,7 +21,7 @@ off_t tsp_util_file_size(const char *path) {
   return tmp.st_size;
 }
 
-int tsp_util_file_last_modify(const char *path, std::string &res) {}
+int tsp_util_file_last_modify(const char *path, string &res) {}
 
 void *tsp_util_malloc(size_t size) {
   void *res = malloc(size);
@@ -33,3 +33,74 @@ void *tsp_util_malloc(size_t size) {
 }
 
 void tsp_util_free(void *p) { free(p); }
+
+int TSPUtilTime::tostring(time_t t, string &res) {
+  struct tm *mtime_tm = localtime(&t);
+  char buff[128];
+  strftime(buff, sizeof(buff), "%F %X", mtime_tm);
+  res = string(buff);
+}
+
+int TSPUtilFile::exist(const char *path) {
+  int res = open(path, O_RDONLY | O_NOFOLLOW), dir = open(path, O_DIRECTORY);
+  if (dir != -1)
+    res = -1;
+  close(dir);
+  close(res);
+  return res == -1 ? -1 : 0;
+}
+
+int TSPUtilFile::size(const char *path, int &size) {
+  struct stat tmp;
+  int res = stat(path, &tmp);
+  if (res == -1) {
+    size = 0;
+    return -1;
+  }
+  size = tmp.st_size;
+  return 0;
+}
+
+int TSPUtilFile::last_modify(const char *path, string &res) {
+  struct stat tmp;
+  int state = stat(path, &tmp);
+  if (state == -1) {
+    res = "";
+    return -1;
+  }
+  TSPUtilTime::tostring(tmp.st_mtime, res);
+  return 0;
+}
+
+int TSPUtilFile::last_access(const char *path, string &res) {
+  struct stat tmp;
+  int state = stat(path, &tmp);
+  if (state == -1) {
+    res = "";
+    return -1;
+  }
+  TSPUtilTime::tostring(tmp.st_atime, res);
+  return 0;
+}
+
+int TSPUtilFile::last_change(const char *path, string &res) {
+  struct stat tmp;
+  int state = stat(path, &tmp);
+  if (state == -1) {
+    res = "";
+    return -1;
+  }
+  TSPUtilTime::tostring(tmp.st_ctime, res);
+  return 0;
+}
+
+void TSPUtilMemory::free(void *p) { free(p); }
+
+void *TSPUtilMemory::malloc(size_t size) {
+  void *res = malloc(size);
+  if (res == NULL) {
+    perror("tsp_util_malloc");
+    exit(-1);
+  }
+  return res;
+}
--- TimeServerPlus/src/utility.h
+++ TimeServerPlus/src/utility.h
@@ -6,11 +6,37 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+using std::string;
+
+// remove in the future,attention please!
+
 int tsp_util_file_exist(const char *path);
 off_t tsp_util_file_size(const char *path);
-int tsp_util_file_last_modify(const char *path, std::string &res);
-int tsp_util_gmt(std::string &res);
+int tsp_util_file_last_modify(const char *path, string &res);
+int tsp_util_gmt(string &res);
 void *tsp_util_malloc(size_t size);
 void tsp_util_free(void *p);
+
+// recommand!
+
+class TSPUtilTime {
+public:
+  static int tostring(time_t t, string &res);
+};
+
+class TSPUtilFile {
+public:
+  static int exist(const char *path);
+  static int size(const char *path, int &size);
+  static int last_modify(const char *path, string &res);
+  static int last_access(const char *path, string &res);
+  static int last_change(const char *path, string &res);
+};
+
+class TSPUtilMemory {
+public:
+  static void free(void *p);
+  static void *malloc(size_t size);
+};
 
 #endif
