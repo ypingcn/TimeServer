d86c0989b9bf541b678417b71a7055a6a3493774
ypingcn <1344632698@qq.com>
2018-03-28 23:13:44 +0800

    refactor(config): add TSPConfig

--- TimeServerPlus/src/config.cpp
+++ TimeServerPlus/src/config.cpp
@@ -1,4 +1,5 @@
 #include "config.h"
+#include "logger.h"
 
 static std::map<std::string, std::string> tsp_config_map;
 
@@ -34,4 +35,50 @@ int tsp_config_get(const std::string key, std::string &value) {
   }
   value = it->second;
   return 0;
+}
+
+TSPConfig *TSPConfig::config = nullptr;
+pthread_mutex_t TSPConfig::lock = PTHREAD_MUTEX_INITIALIZER;
+
+int TSPConfig::parse(const string path) {
+  std::fstream config_file(path, std::fstream::in);
+  if (!config_file) {
+    std::cout << "config error" << std::endl;
+
+    config_file.close();
+    return -1;
+  }
+
+  std::string line;
+  config->data.clear();
+  while (getline(config_file, line)) {
+    std::stringstream stream(line);
+    std::string key, value;
+    stream >> key >> value;
+    if (key != "" && value != "" &&
+        config->data.find("key") == config->data.end())
+      config->data.insert(make_pair(key, value));
+  }
+  config_file.close();
+  return 0;
+}
+
+int TSPConfig::get(const string key, string &value) {
+  std::map<string, string>::const_iterator it = config->data.find(key);
+  if (it == data.end()) {
+    std::cout << "no exist key:" << key << std::endl;
+    return -1;
+  }
+  value = it->second;
+  return 0;
+}
+
+int TSPConfig::exist(const string key, const string value) {
+  std::map<string, string>::const_iterator it = config->data.find(key);
+  std::cout << key << std::endl;
+  if (it == data.end()) {
+    std::cout << "no exist key:" << key << std::endl;
+    return -1;
+  }
+  return it->second == value;
 }
--- TimeServerPlus/src/config.h
+++ TimeServerPlus/src/config.h
@@ -1,14 +1,44 @@
 #ifndef TSP_CONFIG
 #define TSP_CONFIG
 
-#include <map>
-#include <string>
-
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <sstream>
+#include <string>
+
+using std::string;
 
 int tsp_config_parse(const std::string path);
 int tsp_config_get(const std::string key, std::string &value);
 
+class TSPConfig {
+public:
+  TSPConfig(const TSPConfig &) = delete;
+  TSPConfig &operator=(const TSPConfig &) = delete;
+
+  static TSPConfig *instance() {
+    if (config == nullptr) {
+      pthread_mutex_lock(&lock);
+      if (config == nullptr)
+        config = new TSPConfig();
+      pthread_mutex_unlock(&lock);
+    }
+    return config;
+  }
+  int parse(const string path);
+  int get(const string key, string &value);
+  int exist(const string key, const string value);
+
+private:
+  TSPConfig() {
+    data.clear();
+    data.insert(std::make_pair("LOG", "TimeServerPlus.log"));
+  };
+  ~TSPConfig() = default;
+
+  static pthread_mutex_t lock;
+  static TSPConfig *config;
+  std::map<string, string> data;
+};
 #endif
