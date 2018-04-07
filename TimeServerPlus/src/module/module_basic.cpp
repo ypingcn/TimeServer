076b6bdef482d30eadba13341cd37acd07a57dc6
ypingcn <1344632698@qq.com>
2018-04-07 15:42:05 +0800

    refactor(module) : finish module_basic and module_time

--- TimeServerPlus/src/module/module_basic.cpp
+++ TimeServerPlus/src/module/module_basic.cpp
@@ -169,12 +169,127 @@ void *tsp_module_basic_delete(void *sockfd, void *request) {
   tsp_response_headers_end(client_sockfd);
 }
 
-int TSPBasicGetResponse::handle(const TSPRequest &req) {}
-int TSPBasicHeadResponse::handle(const TSPRequest &req) {}
-int TSPBasicOptionsResponse::handle(const TSPRequest &req) {}
-int TSPBasicPostResponse::handle(const TSPRequest &req) {}
-int TSPBasicPutResponse::handle(const TSPRequest &req) {}
-int TSPBasicDeleteResponse::handle(const TSPRequest &req) {}
+// curl 127.0.0.1:8668
+int TSPBasicGetResponse::handle(const TSPRequest &req) {
+  int client_sockfd = req.get_sockfd();
+
+  string path, index;
+  TSPConfig::instance()->get("DOCROOT", path);
+  string file = path + req.get_url();
+  if (req.get_url() == "/")
+    if (TSPConfig::instance()->get("INDEX", index) != -1)
+      file.append(index);
+
+  if (TSPUtilFile::is_directory(file.data()) == 0) {
+    string index;
+    TSPConfig::instance()->get("INDEX", index);
+    if (file[file.length() - 1] == '/')
+      file.append(index);
+    else
+      file.append("/" + index);
+  }
+
+  if (TSPUtilFile::exist(file.data()) == 0) {
+    int fd = open(file.data(), O_RDONLY);
+    off_t size = 0;
+    TSPUtilFile::size(file.data(), size);
+    off_t nwrite = 0;
+    tsp_response_ok(client_sockfd);
+    tsp_response_server_name(client_sockfd);
+    tsp_response_content_length(size, client_sockfd);
+    tsp_response_headers_end(client_sockfd);
+    sendfile(client_sockfd, fd, &nwrite, size);
+    while (nwrite < size) {
+      sendfile(client_sockfd, fd, &nwrite, size);
+    }
+    close(fd);
+  } else {
+    tsp_response_not_found(client_sockfd);
+    tsp_response_server_name(client_sockfd);
+    tsp_response_headers_end(client_sockfd);
+  }
+}
+
+// curl 127.0.0.1:8668 -I
+int TSPBasicHeadResponse::handle(const TSPRequest &req) {
+  int client_sockfd = req.get_sockfd();
+
+  string path, index;
+  TSPConfig::instance()->get("DOCROOT", path);
+  string file = path + req.get_url();
+  if (req.get_url() == "/")
+    if (TSPConfig::instance()->get("INDEX", index) != -1)
+      file.append(index);
+
+  if (TSPUtilFile::is_directory(file.data()) == 0) {
+    string index;
+    TSPConfig::instance()->get("INDEX", index);
+    if (file[file.length() - 1] == '/')
+      file.append(index);
+    else
+      file.append("/" + index);
+  }
+
+  if (TSPUtilFile::exist(file.data()) == 0) {
+    int fd = open(file.data(), O_RDONLY);
+    off_t size = 0;
+    TSPUtilFile::size(file.data(), size);
+    tsp_response_ok(client_sockfd);
+    tsp_response_server_name(client_sockfd);
+    tsp_response_content_length(size, client_sockfd);
+    tsp_response_headers_end(client_sockfd);
+    close(fd);
+  } else {
+    tsp_response_not_found(client_sockfd);
+    tsp_response_server_name(client_sockfd);
+    tsp_response_headers_end(client_sockfd);
+  }
+}
+
+// curl -X OPTIONS 127.0.0.1:8668 -i
+int TSPBasicOptionsResponse::handle(const TSPRequest &req) {
+  int client_sockfd = req.get_sockfd();
+  char allow[1024] = "Allow: OPTIONS, GET, HEAD\r\n";
+  tsp_response_ok(client_sockfd);
+  write(client_sockfd, allow, strlen(allow));
+  tsp_response_date(client_sockfd);
+  tsp_response_server_name(client_sockfd);
+  tsp_response_content_length((off_t)0, client_sockfd);
+  tsp_response_headers_end(client_sockfd);
+}
+
+// curl -X POST 127.0.0.1:8668 -i
+int TSPBasicPostResponse::handle(const TSPRequest &req) {
+  int client_sockfd = req.get_sockfd();
+
+  tsp_response_not_implemented(client_sockfd);
+  tsp_response_date(client_sockfd);
+  tsp_response_server_name(client_sockfd);
+  tsp_response_content_length((off_t)0, client_sockfd);
+  tsp_response_headers_end(client_sockfd);
+}
+
+// curl -X PUT 127.0.0.1:8668 -i -d "body"
+int TSPBasicPutResponse::handle(const TSPRequest &req) {
+  int client_sockfd = req.get_sockfd();
+
+  tsp_response_not_implemented(client_sockfd);
+  tsp_response_date(client_sockfd);
+  tsp_response_server_name(client_sockfd);
+  tsp_response_content_length((off_t)0, client_sockfd);
+  tsp_response_headers_end(client_sockfd);
+}
+
+// curl -X DELETE 127.0.0.1:8668 -i
+int TSPBasicDeleteResponse::handle(const TSPRequest &req) {
+  int client_sockfd = req.get_sockfd();
+
+  tsp_response_not_implemented(client_sockfd);
+  tsp_response_date(client_sockfd);
+  tsp_response_server_name(client_sockfd);
+  tsp_response_content_length((off_t)0, client_sockfd);
+  tsp_response_headers_end(client_sockfd);
+}
 
 void install_basic_modules(ResponseVectorType &t) {
   t.push_back(new TSPBasicGetResponse(MIN_RESPONSE_PRIORITY,
--- TimeServerPlus/src/module/module_time.cpp
+++ TimeServerPlus/src/module/module_time.cpp
@@ -173,10 +173,138 @@ void *tsp_module_time_file_mtime(void *sockfd, void *request) {
   delete client_request;
 }
 
-int TSPTimeLocaltimeResponse::handle(const TSPRequest &req) {}
-int TSPTimeFileCtimeResponse::handle(const TSPRequest &req) {}
-int TSPTimeFileAtimeResponse::handle(const TSPRequest &req) {}
-int TSPTimeFileMtimeResponse::handle(const TSPRequest &req) {}
+int TSPTimeLocaltimeResponse::handle(const TSPRequest &req) {
+  int sockfd = req.get_sockfd();
+  string file = req.get_url();
+
+  tsp_response_ok(sockfd);
+  tsp_response_headers_end(sockfd);
+
+  char body[1024];
+  struct tm *now;
+  time_t timer = time(NULL);
+  now = localtime(&timer);
+  strftime(body, sizeof(body), "<p>Now: %F %X<p>\n", now);
+
+  write(sockfd, body, strlen(body));
+}
+
+int TSPTimeFileCtimeResponse::handle(const TSPRequest &req) {
+  int client_sockfd = req.get_sockfd();
+
+  string path, index;
+  TSPConfig::instance()->get("DOCROOT", path);
+  string file = path + req.get_url();
+  file = file.substr(0, file.find_first_of("?"));
+  if (req.get_url() == "/")
+    if (TSPConfig::instance()->get("INDEX", index) != -1)
+      file.append(index);
+
+  if (TSPUtilFile::is_directory(file.data()) == 0) {
+    string index;
+    TSPConfig::instance()->get("INDEX", index);
+    if (file[file.length() - 1] == '/')
+      file.append(index);
+    else
+      file.append("/" + index);
+  }
+
+  if (TSPUtilFile::exist(file.data()) == 0) {
+    string res;
+    TSPUtilFile::last_change(file.data(), res);
+    res = "<p> " + res + " </p>";
+    off_t size = 0;
+    size = res.length();
+    tsp_response_ok(client_sockfd);
+    tsp_response_server_name(client_sockfd);
+    tsp_response_content_length(size, client_sockfd);
+    tsp_response_headers_end(client_sockfd);
+    TSPUtilIO::basic_write(client_sockfd, const_cast<char *>(res.c_str()),
+                           res.length());
+  } else {
+    tsp_response_not_found(client_sockfd);
+    tsp_response_server_name(client_sockfd);
+    tsp_response_headers_end(client_sockfd);
+  }
+}
+
+int TSPTimeFileAtimeResponse::handle(const TSPRequest &req) {
+  int client_sockfd = req.get_sockfd();
+
+  string path, index;
+  TSPConfig::instance()->get("DOCROOT", path);
+  string file = path + req.get_url();
+  file = file.substr(0, file.find_first_of("?"));
+  if (req.get_url() == "/")
+    if (TSPConfig::instance()->get("INDEX", index) != -1)
+      file.append(index);
+
+  if (TSPUtilFile::is_directory(file.data()) == 0) {
+    string index;
+    TSPConfig::instance()->get("INDEX", index);
+    if (file[file.length() - 1] == '/')
+      file.append(index);
+    else
+      file.append("/" + index);
+  }
+
+  if (TSPUtilFile::exist(file.data()) == 0) {
+    string res;
+    TSPUtilFile::last_access(file.data(), res);
+    res = "<p> " + res + " </p>";
+    off_t size = 0;
+    size = res.length();
+    tsp_response_ok(client_sockfd);
+    tsp_response_server_name(client_sockfd);
+    tsp_response_content_length(size, client_sockfd);
+    tsp_response_headers_end(client_sockfd);
+    TSPUtilIO::basic_write(client_sockfd, const_cast<char *>(res.c_str()),
+                           res.length());
+  } else {
+    tsp_response_not_found(client_sockfd);
+    tsp_response_server_name(client_sockfd);
+    tsp_response_headers_end(client_sockfd);
+  }
+}
+
+int TSPTimeFileMtimeResponse::handle(const TSPRequest &req) {
+  int client_sockfd = req.get_sockfd();
+
+  string path, index;
+  TSPConfig::instance()->get("DOCROOT", path);
+  std::string file = path + req.get_url();
+  file = file.substr(0, file.find_first_of("?"));
+  if (req.get_url() == "/")
+    if (TSPConfig::instance()->get("INDEX", index) != -1)
+      file.append(index);
+
+  if (TSPUtilFile::is_directory(file.data()) == 0) {
+    string index;
+    TSPConfig::instance()->get("INDEX", index);
+    if (file[file.length() - 1] == '/')
+      file.append(index);
+    else
+      file.append("/" + index);
+  }
+
+  if (TSPUtilFile::exist(file.data()) == 0) {
+    string res;
+    TSPUtilFile::last_modify(file.data(), res);
+    res = "<p> " + res + " </p>";
+    off_t size = 0;
+    size = res.length();
+    tsp_response_ok(client_sockfd);
+    tsp_response_server_name(client_sockfd);
+    tsp_response_content_length(size, client_sockfd);
+    tsp_response_headers_end(client_sockfd);
+    TSPUtilIO::basic_write(client_sockfd, const_cast<char *>(res.c_str()),
+                           res.length());
+  } else {
+    tsp_response_not_found(client_sockfd);
+    tsp_response_server_name(client_sockfd);
+    tsp_response_headers_end(client_sockfd);
+  }
+}
 
 void install_time_modules(ResponseVectorType &t) {
   t.push_back(new TSPTimeFileCtimeResponse(
